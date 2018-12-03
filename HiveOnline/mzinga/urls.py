cc0cee918187b7b94336aec30c0e6c00e8562bc4
Tyler Gillson <tyler.gillson@gmail.com>
2018-12-03 14:08:12 -0700

    Integrated w/ SendGrid. Email notifications working.

--- HiveOnline/mzinga/urls.py
+++ HiveOnline/mzinga/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'games', views.GameViewSet)
+router.register(r'games', views.GameViewSet, base_name='games')
 router.register(r'new_game', views.NewGame, base_name='new_game')
 router.register(r'play_move', views.PlayMove, base_name='play_move')
 
--- HiveOnline/mzinga/views.py
+++ HiveOnline/mzinga/views.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -13,6 +15,16 @@ from MzingaShared.Engine import GameEngineConfig
 from MzingaShared.Engine.GameEngine import GameEngine
 
 engine = GameEngine("HiveOnline", GameEngineConfig.get_default_config())
+
+
+# Notify Human player:
+def notify_human_player(g):
+    send_mail(
+        'HiveOnline - Game ID:' + str(g.id),
+        'The ball is in your court!\n\nGame ID: %s\n\nBoard String: %s' % (str(g.id), g.board_string),
+        'noreply@hiveonline.com',
+        [g.player_1.email],
+    )
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -30,9 +42,22 @@ class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
 
+    def get_queryset(self):
+        return Game.objects.all()
+
     def list(self, request, *args, **kwargs):
-        serializer = GameSerializer(self.queryset, many=True, context={'request': request})
+        serializer = GameSerializer(self.get_queryset(), many=True, context={'request': request})
         return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        g = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = GameSerializer(g, many=False, context={'request': request})
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None, *args, **kwargs):
+        g = get_object_or_404(self.get_queryset(), pk=pk)
+        self.perform_destroy(g)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class NewGame(viewsets.ViewSet):
@@ -50,25 +75,30 @@ class NewGame(viewsets.ViewSet):
         }
         :return: JSON serialization of newly created game
         """
+        # Init game, then add players:
         g = Game()
         g.player_1 = request.user
         g.player_2 = User.objects.get_by_natural_key("AI")
 
+        # Extract args from request body:
         ai_config = request.data['ai_config']
         colour = request.data['colour']
 
-        # AI plays first:
+        # AI plays first --> calculate, then play best move according to ai_config:
         if colour == 'Black':
             engine.parse_command("newgame")
             best_move = engine.parse_command("bestmove " + ai_config)
             g.board_string = engine.parse_command("play " + str(best_move))
             g.status = 'InProgress'
+            notify_human_player(g)
 
-        # Human plays first:
+        # Human plays first --> init game:
         elif colour == 'White':
             g.status = 'NotStarted'
             g.board_string = g.status + ";White[1]"
+            notify_human_player(g)
 
+        # Save model instance & return serialized data:
         g.save()
         serializer = GameSerializer(g, many=False, context={'request': request})
         return Response(serializer.data)
